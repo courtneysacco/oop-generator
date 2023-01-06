c23f04f1da7926a6df1467656a031ef38c1e34e4
courtney <xoc0urtney14xo@aim.com>
2023-01-05 21:57:24 -0500

    updated app.js

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const employee = require('./lib/Employee');
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
@@ -9,27 +10,108 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
+console.log('Build your team!');
 
+const employees = [];
 
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
+// questions
 
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
+const questions = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            message: "Submit the employee's name:",
+            name: 'name',
+        },
+        {
+            type: 'input',
+            message: "Submit the employee's ID number:",
+            name: 'id',
+        },
+        {
+            type: 'input',
+            message: "Submit the employee's email address:",
+            name: 'email',
+        },
+        {
+            type: 'list',
+            message: "Select the employee's role:",
+            name: 'role',
+            choices: ['Manager', 'Engineer', 'Intern'],
+        },
+        {
+            // Manager occupation
+            type: 'input',
+            message: "Submit the manager's office number:",
+            name: 'officeNumber',
+            when: (answers) => answers.role === 'Manager',
+        },
+        {
+            // Engineer occupation
+            type: 'input',
+            message: "Submit the engineer's GitHub user:",
+            name: 'github',
+            when: (answers) => answers.role === 'Engineer',
+        },
+        {
+            // Intern occupation
+            type: 'input',
+            message: "Submit the school the intern attends:",
+            name: 'school',
+            when: (answers) => answers.role === 'Intern',
+        },
+        {
+            // If the user selects 'Yes' to add another employee, the questions will run again
+            type: 'confirm',
+            message: 'Would you like to add another employee?',
+            name: 'addEmployee',
+        },
+    ])
+    .then((answers) => {
+        let employee;
 
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
+        if (answers.role === 'Manager') {
+            employee = new Manager(answers.name, answers.id, answers.email, answers.officeNumber);
+        } else if (answers.role === 'Engineer') {
+            employee = new Engineer(answers.name, answers.id, answers.email, answers.github);
+        } else if (answers.role === 'Intern') {
+            employee = new Intern(answers.name, answers.id, answers.email, answers.school);
+        }
+        
+        employees.push(employee);
+        if (answers.addEmployee) {
+            return questions();
+        } else {
+            return employees;
+        }
+    })
+}
 
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
+function writeFileAsync(path, data) {
+    return new Promise((resolve, reject) => {
+        
+        if (!fs.existsSync(OUTPUT_DIR)) {
+            fs.mkdirSync(OUTPUT_DIR);
+        }
+        
+        fs.writeFile(path, data,function (err) {
+            if (err) {
+                reject(err);
+                return;
+            }
+            resolve();
+        });
+    });
+}
 
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
+questions()
+    .then((employees) => {
+        return render(employees);
+    })
+    .then((html) => {
+        return writeFileAsync(outputPath, html);
+    })
+    .then(() => {
+        console.log('Creating team...');
+    })
+    .catch((err) => console.error(err));
